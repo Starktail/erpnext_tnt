ecfafc2ee26fc613feb2988e72faa68354127766
Dirk van der Laarse <dirk@laarse.co.za>
2025-02-03 19:12:37 +0000

    feat: add required doctypes and logic

---
+++ erpnext_tnt/tnt/doctype/tnt_settings/tnt_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Finfoot Tech (Pty) Ltd and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("TNT Settings", {
+// 	refresh(frm) {
+
+// 	},
+// });
