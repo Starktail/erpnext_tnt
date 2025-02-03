f314e70e2f24bf069cc60cf86187ba0f9441ae91
Dirk van der Laarse <dirk@laarse.co.za>
2025-02-03 19:15:00 +0000

    fix: typo in ci

--- .github/helper/install.sh
+++ .github/helper/install.sh
@@ -51,4 +51,4 @@ CI=Yes bench build --app frappe &
 bench --site test_site reinstall --yes
 
 bench --verbose --site test_site install-app erpnext
-bench --verbose --site test_site install-app erpnext-tnt
+bench --verbose --site test_site install-app erpnext_tnt
