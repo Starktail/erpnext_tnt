dee6c1c2b55b08bc28d4d115d751b40912deb8d8
Dirk van der Laarse <dirk@laarse.co.za>
2025-03-24 19:14:02 +0000

    feat: change is-hazardous logic to Shipment in stead of TNT shipment

---
+++ erpnext_tnt/overrides/stock/shipment.py
@@ -0,0 +1,34 @@
+# Copyright (c) 2020, Frappe Technologies Pvt. Ltd. and contributors
+# For license information, please see license.txt
+
+import frappe
+from erpnext.stock.doctype.shipment.shipment import Shipment
+from frappe import _
+
+
+class CustomShipment(Shipment):
+	def validate(self):
+		self.check_if_shipment_contains_hazardous_items()
+		super().validate()
+
+	def on_submit(self):
+		self.validate_custom_tnt_hazardous_weight()
+		super().on_submit()
+
+	def check_if_shipment_contains_hazardous_items(self):
+		"""
+		Check if the linked shipment contains any hazardous items
+		"""
+		for dn in self.shipment_delivery_note:
+			delivery_note = frappe.get_doc("Delivery Note", dn.delivery_note)
+			for row in delivery_note.items:
+				if frappe.get_value("Item", row.item_code, "custom_tnt_is_hazardous"):
+					self.custom_tnt_is_hazardous = True
+					break
+
+	def validate_custom_tnt_hazardous_weight(self):
+		"""
+		Validate the weight of the shipment if it contains hazardous items
+		"""
+		if self.custom_tnt_is_hazardous and self.custom_tnt_hazardous_weight <= 0:
+			frappe.throw(_("Hazardous Weight can not be 0"))
