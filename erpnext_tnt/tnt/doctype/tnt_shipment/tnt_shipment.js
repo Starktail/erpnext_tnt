d943a431982258bcb782cb4703a8d2db05528ea3
Dirk van der Laarse <dirk@laarse.co.za>
2026-05-31 15:40:43 +0000

    feat: override status manually

--- erpnext_tnt/tnt/doctype/tnt_shipment/tnt_shipment.js
+++ erpnext_tnt/tnt/doctype/tnt_shipment/tnt_shipment.js
@@ -38,5 +38,43 @@ frappe.ui.form.on("TNT Shipment", {
         __("API Actions"),
       );
     }
+
+    if (frm.perm[0].write) {
+      frm.add_custom_button(
+        __("Set Status Manually"),
+        function () {
+          frappe.prompt(
+            [
+              {
+                label: __("Status"),
+                fieldname: "status",
+                fieldtype: "Select",
+                options: frm.get_field("status").df.options,
+                default: frm.doc.status,
+                reqd: 1,
+              },
+            ],
+            function (values) {
+              if (values.status === frm.doc.status) {
+                return;
+              }
+              frm.set_value("status", values.status);
+              frm.save().then(() => {
+                frappe.show_alert(
+                  {
+                    message: __("Status updated to {0}", [__(values.status)]),
+                    indicator: "green",
+                  },
+                  5,
+                );
+              });
+            },
+            __("Set Status Manually"),
+            __("Update"),
+          );
+        },
+        __("Actions"),
+      );
+    }
   },
 });
